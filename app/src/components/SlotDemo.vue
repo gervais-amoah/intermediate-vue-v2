efb78ecba4e122379089252f34ef32249ce563ca
Gervais Yao Amoah <gervaisamoah@yahoo.com>
2025-07-15 13:01:17 +0000

    feat: add SlotDemo component to SandboxPage for enhanced slot functionality

---
+++ app/src/components/SlotDemo.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const currentCount = ref(10)
+</script>
+
+<template>
+  <div>
+    <slot name="header">
+      <h2>Slot Demo Title</h2>
+    </slot>
+    <slot :count="currentCount">
+      <p>The count is {{ currentCount }}</p>
+    </slot>
+    <slot name="footer">
+      <p>Default footer content</p>
+    </slot>
+  </div>
+</template>
+
+<style scoped></style>
--- app/src/views/SandboxPage.vue
+++ app/src/views/SandboxPage.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import BaseTextarea from '@/components/BaseTextarea.vue'
-import BaseTextInput from '@/components/BaseTextInput.vue'
+import SlotDemo from '@/components/SlotDemo.vue'
 import { ref } from 'vue'
 
 const firstName = ref<string>('')
@@ -9,22 +8,23 @@ const bio = ref<string>('')
 </script>
 
 <template>
-  <div class="p-8 flex flex-col gap-4">
-    <h1>Sandbox Page</h1>
-    <p>This is a sandbox page for testing components and features.</p>
-    <BaseTextInput
-      id="test-input"
-      label="User name"
-      v-model:first-name="firstName"
-      v-model:last-name="lastName"
-    />
-    <BaseTextarea id="bio-textarea" label="Bio" placeholder="Tell us about you..." v-model="bio" />
-    <p>
-      First name: <strong>{{ firstName }}</strong> <br />
-      Last name: <strong>{{ lastName }}</strong> <br />
-      Bio: <strong>{{ bio }}</strong>
-    </p>
-  </div>
+  <main class="p-8 flex flex-col gap-4">
+    <h1 class="text-2xl">Sandbox Page</h1>
+
+    <SlotDemo>
+      <template #header>
+        <h2>Custom Header</h2>
+      </template>
+
+      <template #default="{ count }">
+        <p>Current count is: {{ count }}</p>
+      </template>
+
+      <template #footer>
+        <h2>Custom Footer</h2>
+      </template>
+    </SlotDemo>
+  </main>
 </template>
 
 <style scoped></style>
