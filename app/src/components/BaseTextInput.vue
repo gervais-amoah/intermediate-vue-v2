3ac2714270defc6bca0411bc37a667f09a89af14
Gervais Yao Amoah <gervaisamoah@yahoo.com>
2025-07-15 11:25:14 +0000

    feat: add BaseTextInput component for text input handling (wip)

---
+++ app/src/components/BaseTextInput.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineOptions({
+  inheritAttrs: false,
+})
+
+defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="form-control">
+    <label class="label" :for="id">
+      <span class="label-text font-medium">{{ label }}</span>
+    </label>
+    <input :id="id" class="input input-bordered w-full" type="text" v-bind="$attrs" />
+  </div>
+</template>
