47156dd11f8803b8924cc840e26c7a8ed30a0008
Gervais Yao Amoah <gervaisamoah@yahoo.com>
2025-07-15 11:55:52 +0000

    feat: add BaseTextarea component for text area input handling

---
+++ app/src/components/BaseTextarea.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineOptions({
+  inheritAttrs: false,
+})
+
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: String,
+    required: true,
+  },
+})
+
+const textareaModel = defineModel<string>('textareaModel')
+</script>
+
+<template>
+  <div class="form-control">
+    <label class="label" :for="id">
+      <span class="label-text font-medium">{{ label }}</span>
+    </label>
+    <textarea
+      :id="id"
+      class="textarea textarea-bordered w-full"
+      rows="3"
+      v-model="textareaModel"
+      v-bind="$attrs"
+    ></textarea>
+  </div>
+</template>
+
+<style scoped></style>
